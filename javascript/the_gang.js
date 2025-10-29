e6812ce9d936db6d95ea911f19f990ba9ffa3746
Violet-nyu <wtg8200@nyu.edu>
2025-10-28 20:21:10 -0400

    js testing

---
+++ javascript/the_gang.js
@@ -0,0 +1,43 @@
+const title = 'Basil Bit and Company, aka "The Gang"'
+
+const people = [
+    {
+        name: "Alkin Fence Brooklyn",
+        description: " - ",
+    },
+    
+    {
+        name: "Basil Bit of Silton",
+        description: " - ",
+    },
+    
+    {
+        name: "Helene Hase von Limmstadt",
+        description: " - ",
+    },
+    
+    {
+        name: "Herb Dower of Brighton",
+        description: " - ",
+    },
+    
+    {
+        name: "Lisbet Lennox",
+        description: " - ",
+    },
+    
+    {
+        name: "Milo",
+        description: " - ",
+    },
+    
+    {
+        name: "Reed",
+        description: " - ",
+    },
+    
+    {
+        name: "Terra",
+        description: " - ",
+    },
+]
