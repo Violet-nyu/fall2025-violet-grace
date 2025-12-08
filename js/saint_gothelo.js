cdbdb7e203c24ea43d1be03225e8128730577461
Violet-nyu <wtg8200@nyu.edu>
2025-12-08 13:50:12 -0500

    Saint Gothelo added, and style testing

---
+++ js/saint_gothelo.js
@@ -0,0 +1,78 @@
+console.log("saint_gothelo");
+
+const saint_gothelo_title = document.createElement("h4");
+saint_gothelo_title.setAttribute("class", "saint_gothelo");
+saint_gothelo_title.textContent = 'Saint Gothelo sur la Montagne and Aix-en-Montagne';
+section.appendChild(saint_gothelo_title);
+
+const saint_gothelo_list = document.createElement("ul");
+saint_gothelo_list.setAttribute("id", "saint_gothelo");
+section.appendChild(saint_gothelo_list);
+
+people.push(
+    {
+        name: "Doctor Giesbert",
+        description: "Doctor Giesbert - The new administrator of the asylum. Arrived about 2 years ago, after which things seem to have sharply declined. A Lothrheimer, who seems to be running experiments of some variety, related to the brain structure which allows humans to hear God.",
+        group: "saint_gothelo",
+    },
+
+    {
+        name: "Gisele Chenemus",
+        description: "Gisele Chenemus - The innkeeper in Aix-en-Montagne, worried about her daughter in the asylum.",
+        group: "saint_gothelo",
+    },
+
+    {
+        name: "Lina Chenemus",
+        description: "Lina Chenemus - The daughter of the innkeeper in Aix-en-Montagne, admitted to the asylum for epilepsy, and unable to be visited recently. A member of the Morons.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Thaddeus",
+        description: "Thaddeus - The leader of 'The Morons', a group within the decayed ward for the insane. Spoke with us, offered us information, and has helped provide us with guidance towards Maristan and doctor Giesbert.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Felix",
+        description: "Felix - One of The Morons, we have not interacted with him directly.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Amorette",
+        description: "Amorette - A ragwoman with The Morons. Did not speak with us, is clearly quite anxious and easily panicked.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Doctor Huron",
+        description: "Doctor Huron - A mouse engineer, abused and looked down on by Doctor Giesbert. Responded well to Herb, and may well be able to be recruited, either temporarily against Doctor Giesbert or more permanently.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Barthélemy",
+        description: "Barthélemy - One of the workers diagnosed with pseudo-bansheeism, the first to speak with us and for the moment something of a leader for the group. We have extended an offer to hire them for to come with us to our castle and retain them as members of our staff, and they seem interested.",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Gaspard",
+        description: "Gaspard - One of the workers diagnosed with pseudo-bansheeism",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Lille-Antoine",
+        description: "Lille-Antoine - One of the workers diagnosed with pseudo-bansheeism",
+        group: "saint_gothelo",
+    },
+    
+    {
+        name: "Dorothée",
+        description: "Dorothée - One of the workers diagnosed with pseudo-bansheeism",
+        group: "saint_gothelo",
+    },
+);
